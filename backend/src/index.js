40e4414af7eb75b7cf4b308a350007dbe7c14885
Divya20085 <divyak.22cse@kongu.edu>
2025-07-03 12:41:53 +0530

    fix: add redirect after login

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,8 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
+
+app.use(cors());
 app.use(
   cors({
     origin: "http://localhost:5173",
